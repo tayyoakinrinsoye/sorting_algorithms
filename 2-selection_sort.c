8bb254f3d94e9f289eda15570b0c17eb3fa31fc3
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-08-17 16:58:59 +0000

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * selection_sort - sort an array in ascending order
+ * @array: array to be sorted
+ * @size: size of array
+ *
+ * Return: void
+ **/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
